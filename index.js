8c458a7d74feb013df63c4be6e9360563f010442
Mukul <mukul.jain@edfora.com>
2018-02-26 13:17:38 +0530

    Add package.json file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import fs from 'fs'
-import AWS from 'aws-sdk'
+const fs = require('fs');
+const AWS = require('aws-sdk');
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 const fileName = 'contacts.csv';
 
